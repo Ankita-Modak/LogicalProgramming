381b8cc360790310ffb074bac304a0a443cf4189
Ankita-Modak <ankitamodak15@gmail.com>
2023-04-21 09:31:45 +0530

    [Ankita]Vending Machine

---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,29 @@
+﻿using LogicalProgramming.Vending_Machine;
+
+namespace LogicalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a program to run-");
+            Console.WriteLine("1. Vending Machine");
+            Console.WriteLine("2. Reverse No.");
+            Console.WriteLine("3. Fibonacci Series");
+
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    vendingmc vendingmc = new vendingmc();
+                    vendingmc.GetNotes();
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter a valid choice");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ LogicalProgramming/Vending Machine/vendingmc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming.Vending_Machine
+{
+    internal class vendingmc
+    {
+        static int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+
+        public static void GetNotes()
+        {
+             
+            
+            Console.Write("Enter the change to be returned: ");
+            int change = int.Parse(Console.ReadLine());
+
+            //calculate no. of notes
+            int[] notesCount = new int[notes.Length];
+            CalculateNotes(change, notesCount, 0);
+
+            int totalNotes = 0;
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (notesCount[i] > 0)
+                {
+                    Console.WriteLine(notesCount[i] + " notes of Rs " + notes[i]);
+                    totalNotes += notesCount[i];
+                }
+            }
+            Console.WriteLine("Total number of notes: " + totalNotes);
+        }
+
+        //calculate minimum number of notes
+        static void CalculateNotes(int change, int[] notesCount, int index)
+        {
+            if (change == 0) return;
+
+            int note = notes[index];
+            int count = change / note;
+
+            if (count > 0)
+            {
+                notesCount[index] = count;
+                change -= note * count;
+            }
+
+            CalculateNotes(change, notesCount, index + 1);
+        }
+    }
+}
+
